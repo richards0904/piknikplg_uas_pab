b92c06adbfbed8873bd8b1b24a747c6fa142dfcc
Richard Satya <113106414+richards0904@users.noreply.github.com>
2023-12-25 04:24:42 +0700

    Tampilan Menu Utama

--- lib/widget/app_color.dart
+++ lib/widget/app_color.dart
@@ -1,3 +1,7 @@
 import 'dart:ui';
 
 final Color primary = Color(0xffff9c4c);
+final Color menu1Color = Color(0xFFf9d263);
+final Color menu2Color = Color(0xFFf2603d);
+final Color menu3Color = Color(0xFF04abe6);
+final Color tabViewColor = Color(0xFFfdfdfd);
